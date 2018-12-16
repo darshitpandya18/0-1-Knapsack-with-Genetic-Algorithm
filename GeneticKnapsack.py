cb4b755aa62dfa4514546522d05fcfedfa4c7c2b
Darshit Pandya <40878465+darshitpandya18@users.noreply.github.com>
2018-12-16 14:18:27 -0800

    Update GeneticKnapsack.py

--- GeneticKnapsack.py
+++ GeneticKnapsack.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 @author: Darshit Pandya
-@title: 0-1 Knapsack Problem solved using Genetic Algorithms
+@title: 0/1 Knapsack Problem solved using Genetic Algorithms
 """
 
 '''
